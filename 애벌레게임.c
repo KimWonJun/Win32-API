8ba68773994a7537b1d906cca8f7a68e3260e5ed
WonJun Kim <kimjnsjwj@gmail.com>
2017-07-18 17:28:48 +0900

    애벌레게임 자동화, 벽, 죽음 추가 (왼쪽 버그)

--- 애벌레게임.c
+++ 애벌레게임.c
@@ -1,7 +1,9 @@
 #include <Windows.h>
+#include <stdlib.h>
 #include <tchar.h>
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
+void End_Game(HWND hwnd, int score);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
 {
@@ -25,8 +27,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 		WS_OVERLAPPEDWINDOW,	//윈도우스타일 (타이틀바의 최소화, 최대화, 닫기) OR기호로 여러개 지정가능
 		CW_USEDEFAULT,		//x좌표
 		CW_USEDEFAULT,		//y좌표
-		CW_USEDEFAULT,		//윈도우 가로크기
-		CW_USEDEFAULT,		//윈도우 세로크기	CW_USEDEFAULT는 커널에 의해 기본값을 지정
+		800,		//윈도우 가로크기
+		500,		//윈도우 세로크기	CW_USEDEFAULT는 커널에 의해 기본값을 지정
 		NULL,	// 부모 윈도우 핸들
 		NULL,	// 메뉴 핸들
 		hInstance,		//응용 프로그램 인스턴스
@@ -43,112 +45,151 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLi
 	return (int)msg.wParam;
 }
 
+void End_Game(HWND hwnd, int score)
+{
+	TCHAR str[100] = { 0, };
+	KillTimer(hwnd, 1);
+	MessageBox(hwnd, _T("사망.."), _T("종료"), MB_OK);
+	wsprintf(str, _T("최총점수 : %d"), score);
+	MessageBox(hwnd, str, _T("최종점수"), MB_OK);
+	PostQuitMessage(0);
+}
+
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 {
 	HDC hdc;
 	PAINTSTRUCT ps;
+	HFONT hFont, oldFont;
 	HPEN hPen, oldPen;
 	static RECT rectView;
+	static TCHAR str[100];
+	static int score;
 	static int coordinates[10][2] = { 0, };
+	static int items[10][2] = { 0, }; 
 	static int circle_cnt;
+	static int direction;
 	int i, x;
-	BOOL chk;
 
 	switch (iMsg)
 	{
 	case WM_CREATE:	//윈도우가 만들어졌을때
 		GetClientRect(hwnd, &rectView);
-		circle_cnt = 2;
-		x = 20;
+		score = 0;
+		direction = VK_RIGHT;
+
+		for (i = 0; i < 10; i++)
+		{
+			items[i][1] = (rand() % 760) + 20;
+			items[i][2] = (rand() % 420) + 20;
+		}
+
+		circle_cnt = 2;	//머리랑 몸
+		x = 30;		//맨처음 x좌표
 		for (i = circle_cnt - 1; i >= 0; i--)
 		{
-			coordinates[i][0] = x, coordinates[i][1] = 20;
-			x += 40;
-		}
+			coordinates[i][0] = x, coordinates[i][1] = 30;
+			x += 20;
+		}	//원들의 좌표설정
+		SetTimer(hwnd, 1, 50, NULL);	//0.05초마다 움직임
 		break;
 
 	case WM_PAINT:
 		hdc = BeginPaint(hwnd, &ps);
-		
-		hPen = (HPEN)CreatePen(PS_SOLID, 1, RGB(0, 0, 255));
-		SelectObject(hdc, hPen);
-		for (i = 1; i < circle_cnt; i++)
-			Ellipse(hdc, coordinates[i][0] - 20, coordinates[i][1] - 20, coordinates[i][0] + 20, coordinates[i][1] + 20);		//나머지
 
-		hPen = (HPEN)CreatePen(PS_SOLID, 1, RGB(255, 0, 0));
+		for (i = 1; i <= 40; i++)
+			Rectangle(hdc, (i - 1) * 20, 0, i * 20, 20);
+		for (i = 2; i <= 39; i++)
+			Rectangle(hdc, (i - 1) * 20, 441, i * 20, 461);
+		for (i = 2; i < 25; i++)
+			Rectangle(hdc, 0, (i - 1) * 20, 20, i * 20);
+		for (i = 2; i < 25; i++)
+			Rectangle(hdc, 764, (i - 1) * 20, 784, i * 20);	//벽그리기
+
+		hPen = (HPEN)CreatePen(PS_SOLID, 1, RGB(0, 0, 255));	//원그리기, 파란색
 		oldPen = SelectObject(hdc, hPen);
-		Ellipse(hdc, coordinates[0][0] - 20, coordinates[0][1] - 20, coordinates[0][0] + 20, coordinates[0][1] + 20);	//머리
+
+		for (i = 1; i < circle_cnt; i++)
+			Ellipse(hdc, coordinates[i][0] - 10, coordinates[i][1] - 10, coordinates[i][0] + 10, coordinates[i][1] + 10);		//나머지
+
+		hPen = (HPEN)CreatePen(PS_SOLID, 1, RGB(255, 0, 0));	//빨강색
+		oldPen = SelectObject(hdc, hPen);
+		Ellipse(hdc, coordinates[0][0] - 10, coordinates[0][1] - 10, coordinates[0][0] + 10, coordinates[0][1] + 10);	//머리
 
 		SelectObject(hdc, oldPen);
-		DeleteObject(hPen);
+		DeleteObject(hPen);		//원그리기
+
+		hFont = CreateFont(20, 0, 0, 0, 0, 0, 0, 0, HANGEUL_CHARSET, 0, 0, 0, 0, _T("나눔바른고딕"));		//점수 출력
+		oldFont = (HFONT)SelectObject(hdc, hFont);
+
+		wsprintf(str, _T("점수 : %d"), score);
+		TextOut(hdc, 0, 0, str, _tcslen(str));
+
+		SelectObject(hdc, oldFont);
+		DeleteObject(hFont);	//점수 출력
+
 		EndPaint(hwnd, &ps);
 		break;
 
 	case WM_KEYDOWN:
-		chk = FALSE;
-		switch (wParam)
+		if (wParam == VK_LEFT || wParam == VK_RIGHT || wParam == VK_UP || wParam == VK_DOWN)
+			direction = wParam;
+		break;
+
+	case WM_TIMER:
+		switch (direction)
 		{
-		case VK_SPACE:
-			if (circle_cnt < 9)
-				circle_cnt++;
-			break;
-		case VK_BACK:
-			if (circle_cnt > 2)
-				circle_cnt--;
-			chk = TRUE;
-			break;
 		case VK_UP:
-			if (coordinates[0][1] - 40 > rectView.top)
+			if (coordinates[0][1] - 20 < rectView.top + 20)
+				End_Game(hwnd, score);
+
+			for (i = circle_cnt - 1; i > 0; i--)
 			{
-				for (i = circle_cnt - 1; i > 0; i--)
-				{
-					coordinates[i][0] = coordinates[i - 1][0];	//x같음
-					coordinates[i][1] = coordinates[i - 1][1];	//y같음
-				}
-				coordinates[0][1] -= 40;
+				coordinates[i][0] = coordinates[i - 1][0];	//x같음
+				coordinates[i][1] = coordinates[i - 1][1];	//y같음
 			}
-			chk = TRUE;
+			coordinates[0][1] -= 20;
 			break;
+
 		case VK_DOWN:
-			if (coordinates[0][1] + 40 < rectView.bottom)
+			if (coordinates[0][1] + 20 > rectView.bottom - 20)
+				End_Game(hwnd, score);
+
+			for (i = circle_cnt - 1; i > 0; i--)
 			{
-				for (i = circle_cnt - 1; i > 0; i--)
-				{
-					coordinates[i][0] = coordinates[i - 1][0];	//x같음
-					coordinates[i][1] = coordinates[i - 1][1];	//y같음
-				}
-				coordinates[0][1] += 40;
+				coordinates[i][0] = coordinates[i - 1][0];	//x같음
+				coordinates[i][1] = coordinates[i - 1][1];	//y같음
 			}
-			chk = TRUE;
+			coordinates[0][1] += 20;
 			break;
+
 		case VK_LEFT:
-			if (coordinates[0][0] - 40 > rectView.left)
+			if (coordinates[0][0] - 20 < rectView.left + 20)
+				End_Game(hwnd, score);
+
+			for (i = circle_cnt - 1; i > 0; i--)
 			{
-				for (i = circle_cnt - 1; i > 0; i--)
-				{
-					coordinates[i][0] = coordinates[i - 1][0];	//x같음
-					coordinates[i][1] = coordinates[i - 1][1];	//y같음
-				}
-				coordinates[0][0] -= 40;
+				coordinates[i][0] = coordinates[i - 1][0];	//x같음
+				coordinates[i][1] = coordinates[i - 1][1];	//y같음
 			}
-			chk = TRUE;
+			coordinates[0][0] -= 20;
 			break;
+
 		case VK_RIGHT:
-			if (coordinates[0][0] + 40 < rectView.right)
+			if (coordinates[0][0] + 20 > rectView.right - 20)
+				End_Game(hwnd, score);
+
+			for (i = circle_cnt - 1; i > 0; i--)
 			{
-				for (i = circle_cnt - 1; i > 0; i--)
-				{
-					coordinates[i][0] = coordinates[i - 1][0];	//x같음
-					coordinates[i][1] = coordinates[i - 1][1];	//y같음
-				}
-				coordinates[0][0] += 40;
+				coordinates[i][0] = coordinates[i - 1][0];	//x같음
+				coordinates[i][1] = coordinates[i - 1][1];	//y같음
 			}
-			chk = TRUE;
+			coordinates[0][0] += 20;
 			break;
 		}
-		if(chk)
-			InvalidateRgn(hwnd, NULL, TRUE);
+		score++;
+		InvalidateRgn(hwnd, NULL, TRUE);
 		break;
+
 	case WM_DESTROY:	//윈도우가 꺼졌을때
 		PostQuitMessage(0);	//GetMessage함수가 0을 반환하게함
 		break;
