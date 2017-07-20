6f271f711a53cfe9db40e10cdd044503264cbfbe
WonJun Kim <kimjnsjwj@gmail.com>
2017-07-20 14:31:00 +0900

    더블버퍼링 적용

--- 애벌레게임.c
+++ 애벌레게임.c
@@ -1,6 +1,7 @@
 #include <Windows.h>
 #include <stdlib.h>
 #include <tchar.h>
+#include <time.h>
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam);
 void End_Game(HWND hwnd, int score);
@@ -60,23 +61,27 @@ void End_Game(HWND hwnd, int score)
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
-{
-	HDC hdc;
+{ 
+	HDC hdc, memdc;
 	PAINTSTRUCT ps;
 	HFONT hFont, oldFont;
 	HPEN hPen, oldPen;
+	HBITMAP hBit, oldBit;
 
 	static RECT rectView;
 	static TCHAR str[100];
 
-	static int coordinates[10][2] = { 0, };
+	static int coordinates[12][2] = { 0, };
 	static int items[10][2] = { 0, }; 
 
 	static int cnt_item;		//아이템 개수
 	static int score;			//점수
 	static int circle_cnt;	//원의 개수
 	static int direction;	//화살표 방향
+	int Speed;
 	int i, x;	//반복문과 x좌표 담을때 사용
+	
+	srand((unsigned)time(NULL));
 
 	switch (iMsg)
 	{
@@ -85,6 +90,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		cnt_item = 10;		//처음 아이템 개수
 		score = 10000;		//처음 점수
 		direction = VK_RIGHT;	//처음 시작 오른쪽으로
+		Speed = 50;
 
 		for (i = 0; i < 10; i++)	//아이템 랜덤배치
 		{
@@ -99,45 +105,55 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 			coordinates[i][0] = x, coordinates[i][1] = 30;
 			x += 20;
 		}	//원들의 좌표설정
-		SetTimer(hwnd, 1, 50, NULL);	//0.05초마다 움직임
+		SetTimer(hwnd, 1, Speed, NULL);	//0.05초마다 움직임
 		break;
 
 	case WM_PAINT:
 		hdc = BeginPaint(hwnd, &ps);
 
+		memdc = CreateCompatibleDC(hdc);
+		hBit = CreateCompatibleBitmap(hdc, 800, 500);
+
+		oldBit = (HBITMAP)SelectObject(memdc, hBit);
+
 		for (i = 1; i <= 40; i++)
-			Rectangle(hdc, (i - 1) * 20, 0, i * 20, 20);
+			Rectangle(memdc, (i - 1) * 20, 0, i * 20, 20);
 		for (i = 2; i <= 39; i++)
-			Rectangle(hdc, (i - 1) * 20, 441, i * 20, 461);
+			Rectangle(memdc, (i - 1) * 20, 441, i * 20, 461);
 		for (i = 2; i < 25; i++)
-			Rectangle(hdc, 0, (i - 1) * 20, 20, i * 20);
+			Rectangle(memdc, 0, (i - 1) * 20, 20, i * 20);
 		for (i = 2; i < 25; i++)
-			Rectangle(hdc, 764, (i - 1) * 20, 784, i * 20);	//벽그리기
+			Rectangle(memdc, 764, (i - 1) * 20, 784, i * 20);	//벽그리기
 
 		for (i = 0; i < 10; i++)
-			Ellipse(hdc, items[i][0] - 10, items[i][1] - 10, items[i][0] + 10, items[i][1] + 10);
+			Ellipse(memdc, items[i][0] - 10, items[i][1] - 10, items[i][0] + 10, items[i][1] + 10);
 
 		hPen = (HPEN)CreatePen(PS_SOLID, 1, RGB(0, 0, 255));	//원그리기, 파란색
-		oldPen =	SelectObject(hdc, hPen);
+		oldPen = (HPEN)SelectObject(memdc, hPen);
 
 		for (i = 1; i < circle_cnt; i++)
-			Ellipse(hdc, coordinates[i][0] - 10, coordinates[i][1] - 10, coordinates[i][0] + 10, coordinates[i][1] + 10);		//나머지
+			Ellipse(memdc, coordinates[i][0] - 10, coordinates[i][1] - 10, coordinates[i][0] + 10, coordinates[i][1] + 10);		//나머지
 
 		hPen = (HPEN)CreatePen(PS_SOLID, 1, RGB(255, 0, 0));	//빨강색
-		oldPen = SelectObject(hdc, hPen);
-		Ellipse(hdc, coordinates[0][0] - 10, coordinates[0][1] - 10, coordinates[0][0] + 10, coordinates[0][1] + 10);	//머리
+		oldPen = (HPEN)SelectObject(memdc, hPen);
+		Ellipse(memdc, coordinates[0][0] - 10, coordinates[0][1] - 10, coordinates[0][0] + 10, coordinates[0][1] + 10);	//머리
 
-		SelectObject(hdc, oldPen);
+		SelectObject(memdc, oldPen);
 		DeleteObject(hPen);		//원그리기
 
 		hFont = CreateFont(20, 0, 0, 0, 0, 0, 0, 0, HANGEUL_CHARSET, 0, 0, 0, 0, _T("나눔바른고딕"));		//점수 출력
-		oldFont = (HFONT)SelectObject(hdc, hFont);
+		oldFont = (HFONT)SelectObject(memdc, hFont);
 
 		wsprintf(str, _T("점수 : %d"), score);
-		TextOut(hdc, 0, 0, str, _tcslen(str));
+		TextOut(memdc, 0, 0, str, _tcslen(str));
 
-		SelectObject(hdc, oldFont);
+		SelectObject(memdc, oldFont);
 		DeleteObject(hFont);	//점수 출력
+
+		BitBlt(hdc, 0, 0, 800, 500, memdc, 0, 0, SRCCOPY);
+
+		SelectObject(memdc, oldBit);
+		DeleteDC(memdc);
 
 		EndPaint(hwnd, &ps);
 		break;
@@ -148,10 +164,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 		break;
 
 	case WM_TIMER:
+
+		for (i = 0; i < 10; i++)
+		{
+			if (coordinates[0][0] == items[i][0] && coordinates[0][1] == items[i][1])
+			{
+				circle_cnt++;		//원 개수 늘리기
+				cnt_item--;	//아이템 개수 줄이기
+				items[i][0] = 0, items[i][1] = 0;		//아이템 초기화
+				score += 100;	//스코어 추가
+			}
+		}
+		score -= 50;		//0.1초마다 50점씩 깎임
+		InvalidateRgn(hwnd, NULL, FALSE);
+
 		if (score == 0)
 			End_Game(hwnd, score);
 		else if (cnt_item == 0)
 			End_Game(hwnd, score);
+
 		switch (direction)
 		{
 		case VK_UP:
@@ -202,18 +233,6 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
 			coordinates[0][0] += 20;
 			break;
 		}
-		for (i = 0; i < 10; i++)
-		{
-			if (coordinates[0][0] == items[i][0] && coordinates[0][1] == items[i][1])
-			{
-				circle_cnt++;		//원 개수 늘리기
-				cnt_item--;	//아이템 개수 줄이기
-				items[i][0] = 0, items[i][1] = 0;		//아이템 초기화
-				score += 100;	//스코어 추가
-			}
-		}
-		score -= 50;		//0.1초마다 50점씩 깎임
-		InvalidateRgn(hwnd, NULL, TRUE);
 		break;
 
 	case WM_DESTROY:	//윈도우가 꺼졌을때
